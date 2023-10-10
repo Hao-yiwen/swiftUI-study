fc23de1f395e98f839aaf75ba4b65c483e266688
haoyiwen <yw.hao@trip.com>
2023-10-10 23:23:58 +0800

    feat: update SwiftUI-study

---
+++ organizing-and-aligning-content-with-stacks/EventTile/EventTileApp.swift
@@ -0,0 +1,14 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+@main
+struct EventTileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ organizing-and-aligning-content-with-stacks/README.md
@@ -0,0 +1,4 @@
+# SwiftUI Concepts
+
+Explore the project for the [Organizing and aligning content with stacks](https://developer.apple.com/tutorials/swiftui-concepts/organizing-and-aligning-content-with-stacks) tutorial.
+
