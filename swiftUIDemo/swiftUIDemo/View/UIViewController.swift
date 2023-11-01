e22a31e0f0715f20eb1fb6949b475863edce4c3f
haoyiwen <yw.hao@trip.com>
2023-11-01 23:02:36 +0800

    feat: add pod and use tg layout view

--- swiftUIDemo/swiftUIDemo/View/UIViewController.swift
+++ swiftUIDemo/swiftUIDemo/View/UIViewController.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import UIKit
+import TangramKit
 
 struct UIKitViewController: UIViewControllerRepresentable {
     typealias UIViewControllerType = UIViewController
@@ -14,6 +15,25 @@ struct UIKitViewController: UIViewControllerRepresentable {
     func makeUIViewController(context: UIViewControllerRepresentableContext<UIKitViewController>) -> UIViewController {
         let viewController = UIViewController()
         viewController.view.backgroundColor = UIColor.red
+        
+        let copyRightView = UILabel()
+        copyRightView.tg_width.equal(.wrap)
+        copyRightView.tg_height.equal(.wrap)
+        copyRightView.tg_top.equal(200)
+        copyRightView.tg_centerX.equal(0)
+        copyRightView.font = UIFont.systemFont(ofSize: 12)
+        copyRightView.textColor = .gray
+        let year = SuperDateUtil.currentYear()
+        copyRightView.text = String(format: "Copyright @c %d Ixuea, All Rights Reserved", year)
+//        copyRightView.text = NSLocalizedString("ErrorNetworkUnknownHost", comment: "")
+//        copyRightView.text = R.string.localizable.errorNetworkUnknownHost()
+        let layout = TGLinearLayout(.vert)
+        layout.tg_width.equal(.fill)
+        layout.tg_height.equal(.fill)
+        layout.addSubview(copyRightView)
+
+        viewController.view.addSubview(layout)
+        
         return viewController
     }
 
